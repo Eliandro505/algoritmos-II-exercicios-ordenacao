da9234a2c3f5d50865fdad943a16f0753b78ccc2
Eliandro <eliandro.gomes42@gmail.com>
2025-05-10 13:26:00 -0300

    Adição do InsertionSort

--- src/br/com/gomes/service/InsertionSort.java
+++ src/br/com/gomes/service/InsertionSort.java
@@ -1,4 +1,18 @@
 package br.com.gomes.service;
 
+import br.com.gomes.util.ArrayUtils;
+
 public class InsertionSort {
+    public static void ordenar(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while(j >= 0 && key < array[j]){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+        ArrayUtils.printArray(array);
+    }
 }
